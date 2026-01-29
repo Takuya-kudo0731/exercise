a0a3c33ab193a89b92ca2094532b1ed97f267bd2
Takuya-kudo0731 <if.i.had.gun@gmail.com>
2026-01-29 10:12:39 +0900

    test.py内容変更

---
+++ test.py
@@ -0,0 +1,13 @@
+
+output = "Hello,world!"
+name = input()
+name = name.lower()
+
+if name == "alice":
+    print(f"Hi, {name}")
+
+elif name == "michael":
+    print(f"I miss you, {name}")
+
+else:
+    print(f"Nice to meet you, {name}")
